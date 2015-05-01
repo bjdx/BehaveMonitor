17be2a430953f5fa74e6870f4d45dede9bb31b66
Gareth Lewis <gareth.lewis91@hotmail.co.uk>
2015-05-01 14:24:46 +0100

    Fixed timing issue and removed JodaTime

--- app/src/main/java/com/example/BehaveMonitor/Event.java
+++ app/src/main/java/com/example/BehaveMonitor/Event.java
@@ -7,9 +7,6 @@ package com.example.BehaveMonitor;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-import org.joda.time.DateTime;
-import org.joda.time.Seconds;
-
 import java.util.Date;
 
 public class Event implements Parcelable{
@@ -58,59 +55,22 @@ public class Event implements Parcelable{
         return note;
     }
 
-    public static final Parcelable.Creator<Event> CREATOR = new Parcelable.Creator<Event>() {
-
-        public Event createFromParcel(Parcel in) {
-            return new Event(in);
-        }
-
-        public Event[] newArray(int size) {
-            return new Event[size];
-        }
-    };
-
-	//Calculates the duration of a state event. Stores it in the format SS.sss
+	// Calculates the duration of a state event. Stores it in the format SS.sss
 	public void end() {
+		long diff = System.currentTimeMillis() - startTime.getTime();
+        int seconds = (int)diff / 1000;
+        diff -= seconds * 1000;
 
-//		long diff = System.currentTimeMillis() - startTime.getTime();
-        DateTime current = new DateTime(), eventTime = new DateTime(startTime);
+        duration = "" + seconds + ".";
+        int length = ("" + diff).length();
 
-        int seconds = Seconds.secondsBetween(eventTime, current).getSeconds();
-        int millis = current.getMillisOfDay() - eventTime.getMillisOfDay();
+        // Pad milliseconds with leading 0s until 3 digits.
+        for (int i = 0; i < 3 - length; i++) {
+            duration += "0";
+        }
 
-        duration = seconds + "." + (millis - (1000 * seconds));
-
-
-//        current = current.minus(new DateTime(startTime).getMillis());
-//        long millis = .getMillis();
-//        SimpleDateFormat sdfSeconds = new SimpleDateFormat("s.SSS");
-//        Date datediff = new Date(diff);
-//        duration = sdfSeconds.format(datediff);
-//        int seconds = (int)diff / 1000;
-//        diff -= seconds * 1000;
-//
-//        duration = "" + seconds + ".";
-//        int length = ("" + diff).length();
-//
-//        switch(length) {
-//            case(0):
-//                duration += "000";
-//                break;
-//            case(1):
-//                duration += "00" + diff;
-//                break;
-//            case(2):
-//                duration += "0" + diff;
-//                break;
-//            case(3):
-//                duration += ""+ diff;
-//                break;
-//            default:
-//                break;
-//        }
+        duration += diff;
 	}
-
-
 
     //Stuff to make it parcelable.
 
@@ -126,4 +86,15 @@ public class Event implements Parcelable{
         dest.writeBooleanArray(new boolean[] {marked});
         dest.writeString(note);
     }
+
+    public static final Parcelable.Creator<Event> CREATOR = new Parcelable.Creator<Event>() {
+
+        public Event createFromParcel(Parcel in) {
+            return new Event(in);
+        }
+
+        public Event[] newArray(int size) {
+            return new Event[size];
+        }
+    };
 }
